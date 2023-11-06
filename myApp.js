834949762308e2074640815b2e3b24c6e8ea8a7d
pedrofaino <pedrofaino99@gmail.com>
2023-11-06 17:30:36 -0300

    index

--- myApp.js
+++ myApp.js
@@ -2,15 +2,14 @@ let express = require('express');
 let app = express();
 
 const absolutePath = __dirname + '/views/index.html'
-
+const path = __dirname + '/public'
+app.use(express.static(path))
 app.get('/',
     function (req, res) {
         res.sendFile(absolutePath)
     }
 )
 
-const path = __dirname + '/public/style.css'
 
-app.use('/public',express.static(path))
 
 module.exports = app;
