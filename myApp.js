e35177e9afbf75d4183fa3fd277fd30b8789ca3e
pedrofaino <pedrofaino99@gmail.com>
2023-11-06 17:49:24 -0300

    index

--- myApp.js
+++ myApp.js
@@ -3,14 +3,14 @@ let app = express();
 
 const absolutePath = __dirname + '/views/index.html'
 const path = __dirname + '/public'
-app.use('/public',express.static(path))
+app.use('/public', express.static(path))
 app.get('/',
     function (req, res) {
         res.sendFile(absolutePath)
     }
 )
-app.get('/json', (res,req) =>{
-    res.json({'message':"Hello Json"})
+app.get('/json', function (res, req) {
+    res.json({ 'message': "Hello Json" })
 })
 
 
