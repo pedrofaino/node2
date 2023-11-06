5c05b0be003b595b0eac4c90a6dcf3ed28f88e9b
pedrofaino <pedrofaino99@gmail.com>
2023-11-06 16:24:08 -0300

    hello

--- myApp.js
+++ myApp.js
@@ -2,7 +2,9 @@ let express = require('express');
 let app = express();
 
 
-console.log("Hello World")
-
-
- module.exports = app;
+app.get('/',
+    function (req, res) {
+        res.send('Hello Express')
+    }
+)
+module.exports = app;
